3b06b402b7cc82653f71fe8c6053288aa71ed025
Adrian Etenyi <etadriano2@gmail.com>
2020-03-31 10:54:20 +0300

    create a new class named quotes

---
+++ src/app/quotes/quotes.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Quotes} from '../quotes';
+@Component({
+  selector: 'app-quotes',
+  templateUrl: './quotes.component.html',
+  styleUrls: ['./quotes.component.css']
+})
+export class QuotesComponent implements OnInit {
+
+
+  quotes: Quotes[] = [
+
+
+  ];
+
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
