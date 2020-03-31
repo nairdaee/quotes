be929d6a84e9158268067f21c5235bf7fb791d63
Adrian Etenyi <etadriano2@gmail.com>
2020-03-31 11:13:44 +0300

    bootstrap my project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { quotes} from './quotes';
+import { Quotes} from './quotes';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
